8bef1430322f097ffd8b784bdd962f53e597ee23
Fabian Canas <fabian@mojotech.com>
2014-09-10 13:01:29 -0400

    Make the saber more like a saber.

--- web/app.js
+++ web/app.js
@@ -12,13 +12,52 @@ document.addEventListener("mousemove", function(e) {
   x = e.x / window.innerWidth;
   y = e.y / window.innerHeight;
 })
-var geometry = new THREE.BoxGeometry(1,10,1);
-var material = new THREE.MeshBasicMaterial( { wireframe: 1 } );
+// var geometry = new THREE.BoxGeometry(1,10,1);
+var geometry = new THREE.PlaneGeometry(4,100);
+geometry.applyMatrix( new THREE.Matrix4().makeRotationY(Math.PI/2) );
+geometry.merge( new THREE.PlaneGeometry(4,100));
+
+
+var generateLaserCanvas = function(){
+	// init canvas
+	var canvas	= document.createElement( 'canvas' );
+	var context	= canvas.getContext( '2d' );
+	canvas.width	= 8;
+	canvas.height	= 16;
+	// set gradient
+	var gradient	= context.createRadialGradient(
+		canvas.width/2, canvas.height /2, 0,
+		canvas.width/2, canvas.height /2, canvas.width /2
+	);		
+	gradient.addColorStop( 0  , 'rgba(255,255,255,0.7)' );
+	gradient.addColorStop( 0.5, 'rgba(192,192,192,0.5)' );
+	gradient.addColorStop( 0.8, 'rgba(128,128,128,0.3)' );
+	gradient.addColorStop( 1  , 'rgba(0,0,0,0)' );
+
+	// fill the rectangle
+	context.fillStyle	= gradient;
+	context.fillRect(0,0, canvas.width, canvas.height);
+	// return the just built canvas 
+	return canvas;	
+};
+var canvas	= generateLaserCanvas();
+	var texture	= new THREE.Texture( canvas );
+	texture.needsUpdate = true;
+var material	= new THREE.MeshBasicMaterial({
+		map		: texture,
+		blending	: THREE.AdditiveBlending,
+		color		: 0xffaacc,
+		side		: THREE.DoubleSide,
+		depthWrite	: false,
+		transparent	: true
+	});
+
+
 var blade = new THREE.Mesh( geometry, material );
 
 scene.add( blade );
 
-camera.position.z = 10;
+camera.position.z = 30;
 
 blade.applyMatrix( new THREE.Matrix4().makeTranslation(0, 5, 0) );
 
